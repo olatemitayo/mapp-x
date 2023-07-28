901ce28a6abf5471be6751d50f5e9bafb1d6ddb8
olatemitayo <[olatunjitemitayo444@gmail.com]>
2023-07-28 10:13:42 +0100

    few modifications

--- components/protected-route.tsx
+++ components/protected-route.tsx
@@ -1,43 +1,9 @@
-// import { useRouter } from "next/router";
-
-// import { useEffect, useState } from "react";
-
-// const withAuth = (WrappedComponent) => {
-//   const Wrapper = (props) => {
-//     const [userObject, setUserObject] = useState({});
-
-//     const router = useRouter();
-
-//     // Perform authentication or authorization check
-
-//     const isAuthenticated = true;
-
-//     useEffect(() => {
-//       setUserObject(JSON.parse(localStorage.getItem("my-user")));
-//     }, []);
-
-//     useEffect(() => {
-//       if (!userObject) {
-//         router.push("/signin"); // Redirect to login page if not authenticated
-//       }
-//     }, [userObject, router]);
-
-//     // Render the wrapped component if authenticated
-
-//     return userObject ? <WrappedComponent {...props} /> : null;
-//   };
-
-//   return Wrapper;
-// };
-
-// export default withAuth;
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 
 const withAuth = (WrappedComponent) => {
   const Wrapper = (props) => {
     const [userObject, setUserObject] = useState(null);
-    // const [isLoading, setIsLoading] = useState(true);
 
     const router = useRouter();
 
@@ -45,7 +11,7 @@ const withAuth = (WrappedComponent) => {
       const checkAuth = () => {
         const user = JSON.parse(localStorage.getItem("my-user"));
         if (!user) {
-          router.push("/signin"); // Redirect to login page if not authenticated
+          router.push("/signin");
         } else {
         }
         setUserObject(user);
